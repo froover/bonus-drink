52cf0f52be2c43507c598110c8e6db1ba5bb2af3
froover <dio22038@gmail.com>
2018-09-06 16:33:19 +0900

    rubyのバージョン更新、テスト、プログラム作成

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,18 @@
 class BonusDrink
+
+  BONUS_AMOUNT = 3
+
   def self.total_count_for(amount)
-    0
+    total_count = amount # 飲んだ合計本数
+
+    while amount >= BONUS_AMOUNT # 空き瓶が３本以下になるまで繰り返す
+      bonus_count = amount / BONUS_AMOUNT #おまけの瓶の本数
+      amount = amount % BONUS_AMOUNT + bonus_count # おまけを貰うのに使わない空き瓶の本数
+      total_count += bonus_count # 空き瓶の本数の合計
+    end
+
+    total_count
   end
 end
+
+puts BonusDrink::total_count_for(100)
